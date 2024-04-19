30e68db316d65f46a1548f05182a5b8ea7f3dd84
rnrathi <rnrathi@users.noreply.github.com>
2024-04-18 20:51:27 -0400

    Added 2 test cases

---
+++ src/test/java/com/test/Regression.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+
+
+public class Regression {
+	
+	@Test
+	public void first() {
+		System.out.println("This is first test case");
+	}
+	
+	@Test
+	public void second() {
+		System.out.println("This is second test case");
+	}
+	
+	
+
+}
